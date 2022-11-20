b723f4d082bc0ea3092f021617a076a891b40caf
Patrick Bucher <patrick.bucher@mailbox.org>
2022-11-20 13:48:27 +0100

    implemented probe component with very limited logging

--- probeCmd/probe.go
+++ probeCmd/probe.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/patrickbucher/meow"
 )
@@ -15,9 +17,85 @@ import (
 func main() {
 	configURL, ok := os.LookupEnv("CONFIG_URL")
 	if !ok {
-		log.Fatalln("environment variable CONFIG_URL must be set")
+		fmt.Fprintln(os.Stderr, "environment variable CONFIG_URL must be set")
+		os.Exit(1)
 	}
-	fmt.Println(mustFetchEndpoints(configURL))
+	endpoints := mustFetchEndpoints(configURL)
+
+	logFileName := fmt.Sprintf("meow-%v.log", time.Now().Format("2006-01-02T15-04-05"))
+	logFilePath := strings.Join([]string{os.TempDir(), logFileName}, string(os.PathSeparator))
+	logFile, err := meow.NewLogFile(logFilePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "open log file %s: %v\n\n", logFilePath, err)
+		os.Exit(1)
+	}
+	defer logFile.Close()
+	fmt.Fprintf(os.Stderr, "started logging to %s\n", logFilePath)
+
+	monitor(endpoints, logFile)
+}
+
+func monitor(endpoints []meow.Endpoint, logger *meow.LogFile) {
+	probe := func(e meow.Endpoint, messages chan string) {
+		messages <- fmt.Sprintf("started logging %s every %v", e.Identifier, e.Frequency)
+		timer := time.NewTimer(e.Frequency)
+		errorCount := 0
+		lastStateOK := false
+		firstTry := true
+		alerted := false
+		for {
+			start := time.Now()
+			status, err := requestForStatus(e)
+			if err != nil {
+				messages <- fmt.Sprintf("request failed: %v", err)
+			}
+			end := time.Now()
+			duration := end.Sub(start)
+			stateOK := status == int(e.StatusOnline)
+			if stateOK {
+				if lastStateOK || firstTry {
+					messages <- fmt.Sprintf("%s is online (took %v)", e.Identifier, duration)
+				} else {
+					messages <- fmt.Sprintf("%s is online again (took %v)", e.Identifier, duration)
+				}
+				lastStateOK = true
+				errorCount = 0
+				alerted = false
+			} else {
+				messages <- fmt.Sprintf("%s is not online", e.Identifier)
+				errorCount++
+				if errorCount >= int(e.FailAfter) && !alerted {
+					messages <- fmt.Sprintf("%s is not online for %d times", e.Identifier, e.FailAfter)
+					alerted = true
+				}
+				lastStateOK = false
+			}
+			firstTry = false
+			<-timer.C
+		}
+	}
+	messages := make(chan string)
+	for _, endpoint := range endpoints {
+		go probe(endpoint, messages)
+	}
+	for logMessage := range messages {
+		// log both to stderr and a log file
+		fmt.Fprintln(os.Stderr, logMessage)
+		logger.WriteLine(logMessage)
+	}
+}
+
+func requestForStatus(e meow.Endpoint) (int, error) {
+	req, err := http.NewRequest(e.Method, e.URL.String(), nil)
+	if err != nil {
+		return 0, fmt.Errorf("prepare request: %s %s %s: %v", e.Identifier, e.Method, e.URL, err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return 0, fmt.Errorf("perform request %s %s %s: %v", e.Identifier, e.Method, e.URL, err)
+	}
+	defer res.Body.Close()
+	return res.StatusCode, nil
 }
 
 func mustFetchEndpoints(configURL string) []meow.Endpoint {
